a585fd3faebdbc35899e8019483c19b10a9fa0f9
mercy-5120 <mgashau18@gmail.com>
2024-03-05 15:59:58 +0300

    I have selected items

---
+++ app.js
@@ -0,0 +1,8 @@
+//selecting items
+let form=document.querySelector(".grocery-form")
+let alert=document.querySelector(".alert")
+let grocery=document.getElementById("grocery")
+let submitbtn=document.querySelector(".submit-btn")
+let container=document.querySelector(".grocery-container")
+let list=document.querySelector(".grocery-list")
+let clearBtn=document.querySelector(".clear-btn")
