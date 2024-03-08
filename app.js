03e954e3ea200ebf7b07975060504e7ffbea6d28
mercy-5120 <mgashau18@gmail.com>
2024-03-08 16:03:18 +0300

    edited options and added event listeners

--- app.js
+++ app.js
@@ -6,3 +6,18 @@ let submitbtn=document.querySelector(".submit-btn")
 let container=document.querySelector(".grocery-container")
 let list=document.querySelector(".grocery-list")
 let clearBtn=document.querySelector(".clear-btn")
+//Edit options
+let editElement
+let editFlag=false
+let editId=""
+//event listeners
+form.addEventListener("submit")
+clearBtn.addEventListener("click")
+//load items
+window.addEventListener("DOMContentLoaded")
+//functions
+function addItems(e){
+    e.preventDefault()
+    const value=grocery.value
+    
+}
